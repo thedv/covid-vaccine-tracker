b48a1c1c10d1e60546d6c7201b6ee27cab2b41cf
Dhruv Patel <86904885+thedv@users.noreply.github.com>
2022-02-05 16:43:26 -0500

    Add files via upload

---
+++ Covid Vaccine Tracker/Database/sells_to.sql
@@ -0,0 +1,56 @@
+create table sells_to (
+	m_id INT,
+	d_id INT,
+    foreign key (m_id) references manufacturers(m_id),
+    foreign key (d_id) references distributor(d_id)
+);
+insert into sells_to (m_id, d_id) values (7, 21);
+insert into sells_to (m_id, d_id) values (9, 12);
+insert into sells_to (m_id, d_id) values (6, 9);
+insert into sells_to (m_id, d_id) values (7, 5);
+insert into sells_to (m_id, d_id) values (7, 7);
+insert into sells_to (m_id, d_id) values (10, 20);
+insert into sells_to (m_id, d_id) values (6, 20);
+insert into sells_to (m_id, d_id) values (3, 12);
+insert into sells_to (m_id, d_id) values (4, 22);
+insert into sells_to (m_id, d_id) values (5, 7);
+insert into sells_to (m_id, d_id) values (5, 3);
+insert into sells_to (m_id, d_id) values (7, 15);
+insert into sells_to (m_id, d_id) values (1, 8);
+insert into sells_to (m_id, d_id) values (9, 10);
+insert into sells_to (m_id, d_id) values (4, 20);
+insert into sells_to (m_id, d_id) values (8, 20);
+insert into sells_to (m_id, d_id) values (9, 20);
+insert into sells_to (m_id, d_id) values (4, 6);
+insert into sells_to (m_id, d_id) values (6, 2);
+insert into sells_to (m_id, d_id) values (4, 11);
+insert into sells_to (m_id, d_id) values (5, 11);
+insert into sells_to (m_id, d_id) values (9, 1);
+insert into sells_to (m_id, d_id) values (6, 1);
+insert into sells_to (m_id, d_id) values (9, 20);
+insert into sells_to (m_id, d_id) values (7, 6);
+insert into sells_to (m_id, d_id) values (2, 25);
+insert into sells_to (m_id, d_id) values (6, 12);
+insert into sells_to (m_id, d_id) values (6, 18);
+insert into sells_to (m_id, d_id) values (5, 15);
+insert into sells_to (m_id, d_id) values (8, 8);
+insert into sells_to (m_id, d_id) values (4, 16);
+insert into sells_to (m_id, d_id) values (1, 13);
+insert into sells_to (m_id, d_id) values (5, 18);
+insert into sells_to (m_id, d_id) values (10, 14);
+insert into sells_to (m_id, d_id) values (10, 7);
+insert into sells_to (m_id, d_id) values (1, 3);
+insert into sells_to (m_id, d_id) values (2, 16);
+insert into sells_to (m_id, d_id) values (10, 14);
+insert into sells_to (m_id, d_id) values (3, 9);
+insert into sells_to (m_id, d_id) values (6, 21);
+insert into sells_to (m_id, d_id) values (10, 22);
+insert into sells_to (m_id, d_id) values (6, 17);
+insert into sells_to (m_id, d_id) values (7, 16);
+insert into sells_to (m_id, d_id) values (9, 7);
+insert into sells_to (m_id, d_id) values (1, 11);
+insert into sells_to (m_id, d_id) values (8, 13);
+insert into sells_to (m_id, d_id) values (5, 3);
+insert into sells_to (m_id, d_id) values (1, 9);
+insert into sells_to (m_id, d_id) values (5, 12);
+insert into sells_to (m_id, d_id) values (6, 10);
